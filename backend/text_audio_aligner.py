ea44d30fcd2ffce9ac367f952594060171624cb2
Yevhenii <85559317+atlantis-11@users.noreply.github.com>
2024-08-14 09:47:14 +0300

    Added progress callbacks to texts aligner and text audio aligner

--- backend/text_audio_aligner.py
+++ backend/text_audio_aligner.py
@@ -50,7 +50,7 @@ def _process_chapter(args):
 
     return idx, intervals
 
-def align_text_with_audio(text_fragments, split_indexes, lang, audio_dir):
+def align_text_with_audio(text_fragments, split_indexes, lang, audio_dir, progress_callback=None):
     chapters = _split_into_chapters(text_fragments, split_indexes)
     audio_files = sorted([os.path.join(audio_dir, f) for f in os.listdir(audio_dir)])
 
@@ -68,6 +68,9 @@ def align_text_with_audio(text_fragments, split_indexes, lang, audio_dir):
         for pr_res in processing_results:
             print(f'Chapter {pr_res[0]} done.')
             chapters_results.append(pr_res)
+
+            if progress_callback != None:
+                progress_callback(len(chapters_results) / len(chapters) * 100)
 
         chapters_results.sort(key=lambda x: x[0])
 
--- backend/texts_aligner.py
+++ backend/texts_aligner.py
@@ -1,4 +1,38 @@
-def align_texts(src_path, tgt_path):
+import sys
+import re
+
+class _ProgressCapturer():
+    def __init__(self, progress_callback):
+        self.progress_callback = progress_callback
+        self.second_progressbar = False
+        self.last_progress = None
+
+    def __enter__(self):
+        if self.progress_callback is not None:
+            self.orig_stderr_write = sys.stderr.write
+            sys.stderr.write = lambda s: (self.orig_stderr_write(s), self.handle_progress(s))
+    
+    def __exit__(self, exc_type, exc_value, tb):
+        if self.progress_callback is not None:
+            sys.stderr.write = self.orig_stderr_write
+
+    def handle_progress(self, str):
+        match = re.search(r'Batches:\s+(\d+)%', str)
+
+        if match:
+            progress = int(match.group(1)) / 2
+
+            if self.last_progress == 50 and progress == 0:
+                self.second_progressbar = True
+
+            if self.second_progressbar:
+                progress += 50
+
+            if progress != self.last_progress:
+                self.last_progress = progress
+                self.progress_callback(progress)
+
+def align_texts(src_path, tgt_path, progress_callback=None):
     from bertalign import Bertalign
 
     with open(src_path, 'r') as f:
@@ -6,7 +40,9 @@ def align_texts(src_path, tgt_path):
     with open(tgt_path, 'r') as f:
         tgt = f.read()
 
-    aligner = Bertalign(src, tgt)
+    with _ProgressCapturer(progress_callback):
+        aligner = Bertalign(src, tgt)
+
     aligner.align_sents()
 
     return aligner.get_result()
