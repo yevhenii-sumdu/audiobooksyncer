bdc2dde92c542ee4661c9856575860c979af43f6
Yevhenii <85559317+atlantis-11@users.noreply.github.com>
2024-09-06 11:32:30 +0300

    Added @cache to get_audio_duration

--- audiobooksyncer/core/utils.py
+++ audiobooksyncer/core/utils.py
@@ -1,4 +1,5 @@
 import os
+from functools import cache
 
 import ffmpeg
 
@@ -7,5 +8,6 @@ def get_sorted_files_in_dir(dir_path):
     return sorted([os.path.join(dir_path, f) for f in os.listdir(dir_path)])
 
 
+@cache
 def get_audio_duration(audio_path):
     return float(ffmpeg.probe(audio_path)['format']['duration'])
