638bea34956016a761da7bedfed34064a5ff1900
Yevhenii <85559317+atlantis-11@users.noreply.github.com>
2025-03-09 22:26:39 +0200

    Added sphinx docs

---
+++ sphinx-docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'audiobooksyncer'
+copyright = '2025, Yevhenii'
+author = 'Yevhenii'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['autoapi.extension', 'sphinx_rtd_theme']
+
+autoapi_dirs = ['../audiobooksyncer']
+autoapi_options = [
+    'members',
+    'undoc-members',
+    # 'private-members',
+    'show-inheritance',
+    'show-module-summary',
+    # 'special-members',
+    'imported-members',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ sphinx-docs/index.rst
@@ -0,0 +1,10 @@
+audiobooksyncer documentation
+=============================
+
+An application to synchronize an audiobook with text and its translation.
+
+The purpose of the app is to simplify the experience of listening to audiobooks in a foreign language, so you can quickly look up what's being said and check the translation if needed.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
