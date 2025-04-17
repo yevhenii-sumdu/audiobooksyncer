50e6e11fa505828a1493bb2c52de3a073aa6217d
Yevhenii <85559317+atlantis-11@users.noreply.github.com>
2025-04-17 22:47:03 +0300

    Added an option to set logging level via env var

--- audiobooksyncer/__main__.py
+++ audiobooksyncer/__main__.py
@@ -1,3 +1,5 @@
+import os
+
 from loguru import logger
 
 from .scripts.cli import main
@@ -6,7 +8,7 @@ logger.remove()  # remove default logger
 
 logger.add(
     'app.log',
-    level='DEBUG',
+    level=os.getenv('LOGGING_LEVEL', 'DEBUG'),
     format='{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}',
 )
 
